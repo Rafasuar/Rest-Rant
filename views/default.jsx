5d272d5d3c5db07ae3486ebf57414dbd5271dfa9
Rafasuar <suarez.rafael.a@outlook.com>
2022-09-15 20:22:32 -0700

    adding homepage view

--- views/default.jsx
+++ views/default.jsx
@@ -1,1 +1,15 @@
 const React = require('react')
+function Def (html) {
+    return (
+        <html>
+            <head>
+                <title>Rest-Rant</title>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    )
+}
+
+module.exports = Def
