3a1a50bde386c1c68ea812d594a292d1f8266d6c
Rafasuar <suarez.rafael.a@outlook.com>
2022-09-29 17:56:00 -0700

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -47,12 +47,25 @@ router.get('/:id',(req,res) => {
     if (isNaN(id)) {
         res.render('error404')
     }
-     else if (!places[id]) {
+    else if (!places[id]) {
         res.render('error404')
     }
-     else {
-        res.render('places/show',{place:places[id]})
+    else {
+        res.render('places/show', { place: places[id], id})
     }
 })
-
+// DELETE //
+router.delete('/:id',(req,res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        places.splice(id,1)
+        res.redirect('/places')
+    }
+})
 module.exports = router
